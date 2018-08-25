a43310d16d1200ed552fe1c4d5463d3e31df1a17
Mike Shafter <yuhang.061@gmail.com>
2018-08-25 12:48:41 +0800

    added comments

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ root = Tk()
 root.title('Budget Planner')
 root.geometry("500x500")
 
+# Functions for buttons
 def calcmein():
   calc.mein()
 def budgetmein():
   budget.mein()
 
-budgetb = Button(root,text="Change budget",command=budgetmein).place(relx=0.5, rely=0.4, anchor=CENTER)
-calcb = Button(root,text="Calculator",command=calcmein).place(relx=0.5, rely=0.6, anchor=CENTER)
+budgetb = Button(root,text="Change budget",command=budgetmein).place(relx=0.5, rely=0.4, anchor=CENTER) # change budget button
+calcb = Button(root,text="Calculator",command=calcmein).place(relx=0.5, rely=0.6, anchor=CENTER) # Calculator button
 
 root.mainloop()

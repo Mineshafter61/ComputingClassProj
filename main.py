b90e5d947ba28abda6eace7a70ece11d1ac68900
Mike Shafter <yuhang.061@gmail.com>
2019-01-10 09:37:50 +0800

    Added exit button

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def itemmein():
 # What should I buy less?
 def buymein():
   count=[]
-  categories=("Antiques", "Art", "Baby","Books","Business & Industrial","Cameras & Photo","Cell Phones & Accessories","Clothing, Shoes & Accessories","Coins & Paper Money","Collectibles","Computers/Tablets & Networking","Consumer Electronics","Crafts","Dolls & Bears","DVDs & Movies","Entertainment Memorabilia","Gift Cards & Coupons","Health & Beauty","Home & Garden","Jewelry & Watches","Music","Musical Instruments & Gear","Pet Supplies","Pottery & Glass","Real Estate","Specialty Services","Sporting Goods","Sports Mem, Cards & Fan Shop","Stamps","Tickets & Experiences","Toys & Hobbies","Travel","Video Games & Consoles","Everything Else")
+  categories=("Nothing","Antiques", "Art", "Baby","Books","Business & Industrial","Cameras & Photo","Cell Phones & Accessories","Clothing, Shoes & Accessories","Coins & Paper Money","Collectibles","Computers/Tablets & Networking","Consumer Electronics","Crafts","Dolls & Bears","DVDs & Movies","Entertainment Memorabilia","Gift Cards & Coupons","Health & Beauty","Home & Garden","Jewelry & Watches","Music","Musical Instruments & Gear","Pet Supplies","Pottery & Glass","Real Estate","Specialty Services","Sporting Goods","Sports Mem, Cards & Fan Shop","Stamps","Tickets & Experiences","Toys & Hobbies","Travel","Video Games & Consoles","Everything Else")
   prices = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   itemlist = open('itemlist.txt','r').read() # The entire item list
   itemlist = itemlist.split('\n')
@@ -43,9 +43,15 @@ else:
 
 buy = Label(root, justify=CENTER, padx=10, text=str("You should cut down on buying items from this category:\n" + nobuy)).place(relx=0.5, rely=0.3, anchor=CENTER) # Show which item to buy less
 
+#Close and start programs
+def calcopen():
+  calcmein()
+  self.root.destroy()
+
 budgetb = Button(root,text="Change budget",command=budgetmein).place(relx=0.5, rely=0.4, anchor=CENTER) # Change Budget button
 calcb = Button(root,text="Calculator",command=calcmein).place(relx=0.5, rely=0.5, anchor=CENTER) # Calculator button
 newitemb = Button(root,text="New Item...",command=itemmein).place(relx=0.5,rely=0.6,anchor=CENTER) # New Item button
 buyless = Button(root,text="What should I stop buying?",command=buymein).place(relx=0.5,rely=0.7,anchor=CENTER) # Calculate buy less item
+close = Button(root,text="Close window",command=exit).place(relx=0.5,rely=0.8,anchor=CENTER) # Calculate buy less item
 
 root.mainloop()
